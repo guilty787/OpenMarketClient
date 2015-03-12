0a18b084ef7583b674120b4903ffa86fd260a27e
Gil Moshayof <gilm@wix.com>
2015-03-12 22:35:02 +0200

    added dummy user & item data

---
+++ app/scripts/services/graph.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name openMarketClientApp.Graph
+ * @description
+ * # Graph
+ * Service in the openMarketClientApp.
+ */
+angular.module('openMarketClientApp')
+  .service('Graph', function () {
+
+
+    this.findOptimalPath = function (item, options) {
+
+
+    };
+
+  });
